0c395e75a14765d9ed98c53fed503164e257214c
Mohamed Mahmoud <109958214+m7bdi3@users.noreply.github.com>
2023-03-21 21:59:43 +0200

    fixed env variables

--- udagram/udagram-frontend/dist/out-tsc/src/environments/environment.prod.js
+++ udagram/udagram-frontend/dist/out-tsc/src/environments/environment.prod.js
@@ -1,13 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: true,
-  appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
+export var environment = {
+    production: true,
+    appName: 'Udagram',
+    apiHost: 'http://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -16,3 +14,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+//# sourceMappingURL=environment.prod.js.map
